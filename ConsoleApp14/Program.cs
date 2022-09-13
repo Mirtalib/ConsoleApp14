a3eda74aa4cc296620006ce39c9c2f5ffd789022
Mirtalib <108764438+Mirtalib@users.noreply.github.com>
2022-09-14 00:08:53 +0400

    Proje dosyası ekle.

---
+++ ConsoleApp14/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace ConsoleApp14;
+
+class Run
+{
+    public void runFunc(Func del, string s) => del(s);
+}
+
+
+class MyClass
+{
+    public string S;
+    public MyClass(string s)
+    {
+        S = s;
+    }
+
+    public void Space(string s)
+    {
+        Console.WriteLine($"\n{string.Join("_", s.ToCharArray())}\n");
+    }
+    public void Reverse(string s)
+    {
+        for (int i = s.Length - 1; i >= 0; i--)
+        {
+            Console.Write(s[i]);
+        }
+    }
+}
+
+delegate void Func(string str);
+
+
+
+class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("Enter string");
+        var str = Console.ReadLine()!;
+        MyClass cls = new MyClass(str!);
+        Func funcDell = new Func(cls.Reverse); 
+        funcDell += cls.Space;
+        Run run = new Run();
+        run.runFunc(funcDell, str);
+    }
+}
